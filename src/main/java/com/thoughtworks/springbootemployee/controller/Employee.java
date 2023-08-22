f2da6dd535ae533996d7ce1b93964e3bbe19fbb2
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-23 02:21:12 +0800

    refactor: changed variable data type to final and removed unused methods in Employee.java

--- src/main/java/com/thoughtworks/springbootemployee/controller/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/Employee.java
@@ -45,20 +45,8 @@ public class Employee {
         return salary;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setAge(Integer age) {
         this.age = age;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
     }
 
     public void setSalary(Integer salary) {
