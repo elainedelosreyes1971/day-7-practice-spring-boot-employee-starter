6d1d4580b7076798873368950896efb3c362353d
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-24 01:37:34 +0800

    refactor: formatted code

--- src/test/java/com/thoughtworks/springbootemployee/CompanyApiTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyApiTest.java
@@ -45,6 +45,7 @@ public class CompanyApiTest {
                 .andExpect(jsonPath("$[0].id").value(newCompany.getId()))
                 .andExpect(jsonPath("$[0].name").value(newCompany.getName()));
     }
+
     @Test
     void should_return_the_company_when_perform_get_company_given_company_id() throws Exception {
         //given
