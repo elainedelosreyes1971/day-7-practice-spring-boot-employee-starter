beafde7c975a9e81bb84cf8f4a81c8d4fac0c40d
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-24 10:00:28 +0800

    refactor: added @SuppressWarnings all as commented by Daphne

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@SuppressWarnings("all")
 @RequestMapping("/companies")
 @RestController
 public class CompanyController {
--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@SuppressWarnings("all")
 @RequestMapping("/employees")
 @RestController
 public class EmployeeController {
